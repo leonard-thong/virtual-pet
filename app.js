8087cf528e851e69a1af42bb48a9ecaa4d5392f7
Leonard Thong <leonard@lawn-143-215-126-205.lawn.gatech.edu>
2023-03-04 18:47:08 -0500

    Add reset function

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.get('/tasks', (req, res) => {
   res.send({ tasks })
 })
 
+app.post('/reset', (req, res) => {
+  level = 1
+  tasks = [{id: 1, title: "Take a walk"}, {id: 2, title: "Water the flower"}, {id: 3, title: "Clean the dishes"}]
+  res.send('Reset successful');
+})
+
 app.delete('/tasks/:id', (req, res) => {
   id = req.params.id
   tasks = tasks.filter(x => {return x.id != id})
